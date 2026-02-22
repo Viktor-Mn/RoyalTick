32b0b4fe4620221afc372096f5cf262319c3dda7
ViktorMinaylenko <ipz224_mvyu@student.ztu.edu.ua>
2026-02-22 21:48:26 +0200

    Move .git to root, preserve shop history, and add admin folder

---
+++ admin/test-admin/components/ProductShow/ProductShow.tsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import {
+  Show,
+  SimpleShowLayout,
+  TextField,
+  RichTextField,
+  ImageField,
+  DeleteButton,
+} from "react-admin";
+import "./index.css";
+
+export const ProductShow = () => {
+  return (
+    <Show>
+      <SimpleShowLayout>
+        <div className="show-actions">
+          <DeleteButton />
+        </div>
+        <ImageField source="images" src="url" title="desc" />
+        <TextField source="name" />
+        <TextField source="category" />
+        <RichTextField source="description" />
+      </SimpleShowLayout>
+    </Show>
+  );
+};
---
+++ admin/test-admin/src/dataProvider.ts
@@ -0,0 +1,137 @@
+import { DataProvider, fetchUtils } from "react-admin";
+import { stringify } from "query-string";
+import axios from "axios";
+
+const apiUrl = "http://localhost:3000/api/admin";
+const httpClient = fetchUtils.fetchJson;
+
+export default {
+  getList: async (resource, params) => {
+    const { page = 1, perPage = 10 } = params.pagination ?? {};
+    const { field = "id", order = "ASC" } = params.sort ?? {};
+
+    const query = {
+      sort: JSON.stringify([field, order]),
+      range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+      filter: JSON.stringify(params.filter ?? {}),
+    };
+
+    const url = `${apiUrl}/${resource}/list?${stringify(query)}`;
+    const { data } = await axios.get(url);
+
+    console.log("Data received from API:", data);
+
+    return {
+      data: data.items,
+      total: data.count,
+    };
+  },
+
+  getOne: async (resource, params) => {
+    const rawCategory = localStorage.getItem("show");
+    const category = rawCategory ? JSON.parse(rawCategory) : "";
+
+    const url = `${apiUrl}/${resource}/one?id=${params.id}&category=${category}`;
+
+    const { data } = await axios.get(url);
+
+    return {
+      data: data.productItem,
+    };
+  },
+
+  getMany: async (resource, params) => {
+    const query = {
+      filter: JSON.stringify({ ids: params.ids }),
+    };
+    const url = `${apiUrl}/${resource}?${stringify(query)}`;
+    const { json } = await httpClient(url);
+    return { data: json };
+  },
+
+  getManyReference: async (resource, params) => {
+    const { page, perPage } = params.pagination;
+    const { field, order } = params.sort;
+    const query = {
+      sort: JSON.stringify([field, order]),
+      range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+      filter: JSON.stringify({
+        ...params.filter,
+        [params.target]: params.id,
+      }),
+    };
+    const url = `${apiUrl}/${resource}?${stringify(query)}`;
+    const { json, headers } = await httpClient(url);
+    const contentRange = headers.get("content-range");
+
+    const total = contentRange
+      ? parseInt(contentRange.split("/").pop() ?? "0", 10)
+      : 0;
+
+    return {
+      data: json,
+      total,
+    };
+  },
+
+  create: async (resource, params) => {
+    const { json } = await httpClient(`${apiUrl}/${resource}`, {
+      method: "POST",
+      body: JSON.stringify(params.data),
+    });
+    return { data: json };
+  },
+
+  update: async (resource, params) => {
+    const url = `${apiUrl}/${resource}/${params.id}`;
+    const { json } = await httpClient(url, {
+      method: "PUT",
+      body: JSON.stringify(params.data),
+    });
+    return { data: json };
+  },
+
+  updateMany: async (resource, params) => {
+    const query = {
+      filter: JSON.stringify({ id: params.ids }),
+    };
+    const url = `${apiUrl}/${resource}?${stringify(query)}`;
+    const { json } = await httpClient(url, {
+      method: "PUT",
+      body: JSON.stringify(params.data),
+    });
+    return { data: json };
+  },
+
+  delete: async (resource, params) => {
+    if (!params.previousData) {
+      throw new Error("No previousData provided for delete operation");
+    }
+
+    const id = params.previousData.id;
+    let category = params.previousData.category;
+
+    const categoryMap: Record<string, string> = {
+      watches: "watches",
+      straps: "straps",
+      accessories: "boxes",
+    };
+
+    const mongoCollection = categoryMap[category] ?? category;
+
+    const url = `${apiUrl}/${resource}/delete?id=${id}&category=${mongoCollection}`;
+    const { data } = await axios.get(url);
+
+    return { data };
+  },
+
+  deleteMany: async (resource, params) => {
+    const url = `${apiUrl}/${resource}/delete-many?ids=${JSON.stringify(params.ids)}`;
+
+    await axios.get(url);
+
+    return {
+      data: [],
+    };
+  },
+} as DataProvider;
